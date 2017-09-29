3bee0e666772eb89012b7fd9df070ffa6fffc9de
Ramon Rabello <ramon.rabello@colab.re>
2017-09-29 03:22:21 -0300

    added views binding with ButterKnife and customization of toolbar title.

--- app/src/main/java/com/github/ramonrabello/newsfeed/MainActivity.java
+++ app/src/main/java/com/github/ramonrabello/newsfeed/MainActivity.java
@@ -2,12 +2,30 @@ package com.github.ramonrabello.newsfeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.github.ramonrabello.newsfeed.common.TextUtils;
+import com.github.ramonrabello.newsfeed.news.NewsFeedFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        TextUtils.spanToolbarTitle(this, toolbar.getTitle(), 25, getSupportActionBar());
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, NewsFeedFragment.newInstance()).addToBackStack(null).commit();
+        }
     }
 }
