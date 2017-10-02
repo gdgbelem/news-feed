a5bed3767493a86c62d12197c5e7b836e7426089
Ramon Rabello <ramon.rabello@colab.re>
2017-10-02 04:05:39 -0300

    changed class to use BaseActivity architecture.

--- app/src/main/java/com/github/ramonrabello/newsfeed/MainActivity.java
+++ app/src/main/java/com/github/ramonrabello/newsfeed/MainActivity.java
@@ -1,16 +1,14 @@
 package com.github.ramonrabello.newsfeed;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-import com.github.ramonrabello.newsfeed.common.TextUtils;
+import com.github.ramonrabello.newsfeed.common.BaseActivity;
 import com.github.ramonrabello.newsfeed.news.NewsFeedFragment;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -19,10 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        TextUtils.spanToolbarTitle(this, toolbar.getTitle(), 25, getSupportActionBar());
 
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, NewsFeedFragment.newInstance()).addToBackStack(null).commit();
