b502484be4388d57b263abe1637b49dc3d25b518
Ramon Rabello <ramon.rabello@colab.re>
2017-10-02 03:10:34 -0300

    added parcelable implementation.

--- app/src/main/java/com/github/ramonrabello/newsfeed/news/FeedItem.java
+++ app/src/main/java/com/github/ramonrabello/newsfeed/news/FeedItem.java
@@ -1,11 +1,14 @@
 package com.github.ramonrabello.newsfeed.news;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Model that represents a feed item in news feed.
  */
-class FeedItem {
+public class FeedItem implements Parcelable{
     private String type;
     private String title;
     private String thumb;
@@ -17,9 +20,32 @@ class FeedItem {
     @SerializedName("webview-url")
     private String webviewUrl;
 
+    protected FeedItem(Parcel in) {
+        type = in.readString();
+        title = in.readString();
+        thumb = in.readString();
+        updated = in.readLong();
+        shareUrl = in.readString();
+        webviewUrl = in.readString();
+    }
+
+    public static final Creator<FeedItem> CREATOR = new Creator<FeedItem>() {
+        @Override
+        public FeedItem createFromParcel(Parcel in) {
+            return new FeedItem(in);
+        }
+
+        @Override
+        public FeedItem[] newArray(int size) {
+            return new FeedItem[size];
+        }
+    };
+
     public boolean withThumb(){
         return thumb != null;
     }
+
+    public boolean withShareUrl() { return shareUrl != null; }
 
     public String getType() {
         return type;
@@ -67,5 +93,20 @@ class FeedItem {
 
     public void setWebviewUrl(String webviewUrl) {
         this.webviewUrl = webviewUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(type);
+        parcel.writeString(title);
+        parcel.writeString(thumb);
+        parcel.writeLong(updated);
+        parcel.writeString(shareUrl);
+        parcel.writeString(webviewUrl);
     }
 }
