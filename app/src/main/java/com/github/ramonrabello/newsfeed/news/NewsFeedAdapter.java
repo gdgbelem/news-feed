4da8d17f835d99c370b4b99142eb0595296686f2
Ramon Rabello <ramon.rabello@colab.re>
2017-10-02 04:03:59 -0300

    changed adapter.

--- app/src/main/java/com/github/ramonrabello/newsfeed/news/NewsFeedAdapter.java
+++ app/src/main/java/com/github/ramonrabello/newsfeed/news/NewsFeedAdapter.java
@@ -1,48 +1,42 @@
 package com.github.ramonrabello.newsfeed.news;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.content.ContextCompat;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.github.ramonrabello.newsfeed.R;
-import com.github.ramonrabello.newsfeed.common.TextUtils;
+import com.github.ramonrabello.newsfeed.detail.NewsDetailActivity;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Locale;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 /**
  * Adapter for news feed.
  */
-public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.FeedItemViewHolder> {
+class NewsFeedAdapter extends RecyclerView.Adapter<FeedItemViewHolder> implements FeedItemViewHolder.OnFeedItemClickListener {
 
     private List<FeedItem> feedItems;
+    private Context context;
 
-    public NewsFeedAdapter() {
+    NewsFeedAdapter() {
         feedItems = new ArrayList<>();
     }
 
     @Override
-    public NewsFeedAdapter.FeedItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item_view_holder, parent, false);
-        return new FeedItemViewHolder(itemView);
+    public FeedItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        this.context = parent.getContext();
+        View itemView = LayoutInflater.from(context).inflate(R.layout.feed_item_view_holder, parent, false);
+        FeedItemViewHolder viewHolder = new FeedItemViewHolder(itemView);
+        viewHolder.setOnFeedItemClickListener(this);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(NewsFeedAdapter.FeedItemViewHolder holder, int position) {
+    public void onBindViewHolder(FeedItemViewHolder holder, int position) {
         if (!feedItems.isEmpty()){
             holder.bind(feedItems.get(position));
         }
@@ -60,44 +54,11 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.FeedIt
         }
     }
 
-    /**
-     * View holder for feed items.
-     */
-    class FeedItemViewHolder extends BaseViewHolder<FeedItem> {
-
-        @BindView(R.id.feed_item_title)
-        TextView feedItemTitle;
-
-        @BindView(R.id.feed_item_time)
-        TextView feedItemUpdated;
-
-        @BindView(R.id.feed_item_thumb_image)
-        ImageView feedItemThumbImage;
-
-        FeedItemViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-            TextUtils.asRegular(getAssets(), feedItemTitle);
-            TextUtils.asLight(getAssets(), feedItemUpdated);
-        }
-
-        @Override
-        public void bind(FeedItem feedItem) {
-            itemView.setTag(feedItem);
-            feedItemTitle.setText(feedItem.getTitle());
-            feedItemUpdated.setText(new UpdatedTimeFormatter().format(feedItem.getUpdated()));
-
-            if (feedItem.withThumb()){
-                if (!feedItemThumbImage.isShown()){
-                    feedItemThumbImage.setVisibility(View.VISIBLE);
-                }
-                Glide.with(getContext()).load(feedItem.getThumb()).
-                        apply(RequestOptions.placeholderOf(new ColorDrawable(ContextCompat.getColor(getContext(), R.color.grayLineDivider))))
-                        .apply(RequestOptions.centerCropTransform())
-                        .into(feedItemThumbImage);
-            } else {
-                feedItemThumbImage.setVisibility(View.GONE);
-            }
-        }
+    @Override
+    public void onFeedItemClick(int position) {
+        Intent intent = new Intent(context, NewsDetailActivity.class);
+        FeedItem feedItem = feedItems.get(position);
+        intent.putExtra(NewsDetailActivity.EXTRA_FEED_ITEM, feedItem);
+        context.startActivity(intent);
     }
 }
